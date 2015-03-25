418e8ec52887c8196fbd6208928b2cb768b05f20
juanonsoftware <huanhvhd@gmail.com>
2015-03-25 19:18:12 +0700

    Add a sample on RavenDB

---
+++ HelloRavenDB/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HelloRavenDB.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HelloRavenDB.Controllers
+{
+    public class HomeController : RavenController
+    {
+        public ActionResult InitSamples()
+        {
+            var book1 = new Book()
+            {
+                Author = "Huan",
+                Id = Guid.NewGuid(),
+                Title = "Book ONE"
+            };
+
+            RavenSession.Store(book1);
+
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var books = RavenSession.Query<Book>().ToList();
+            return View(books);
+        }
+
+    }
+}
---
+++ HelloRavenDB/Controllers/RavenController.cs
@@ -0,0 +1,54 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System.Web.Mvc;
+
+namespace HelloRavenDB.Controllers
+{
+    public abstract class RavenController : Controller
+    {
+        private static IDocumentStore _documentStore;
+
+        public static IDocumentStore DocumentStore
+        {
+            get
+            {
+                return _documentStore ?? (_documentStore = new DocumentStore()
+                {
+                    ConnectionStringName = "ravenDB"
+                }.Initialize());
+            }
+        }
+
+        public IDocumentSession RavenSession { get; protected set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            RavenSession = DocumentStore.OpenSession();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+                return;
+
+            using (RavenSession)
+            {
+                if (filterContext.Exception != null)
+                    return;
+
+                if (RavenSession != null)
+                    RavenSession.SaveChanges();
+            }
+        }
+
+        protected HttpStatusCodeResult HttpNotModified()
+        {
+            return new HttpStatusCodeResult(304);
+        }
+
+        protected new JsonResult Json(object data)
+        {
+            return base.Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
